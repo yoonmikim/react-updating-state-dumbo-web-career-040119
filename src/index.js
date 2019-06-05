473561f15231c28cf6f22103cfe2e0399f10400f
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-06-05 15:28:00 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-  <div>
-    Mount Components Here
-  </div>,
-  document.getElementById('root')
-);
+ 
+import ClickityClick from './components/ClickityClick';
+ 
+ReactDOM.render(<ClickityClick />, document.getElementById('root'));
